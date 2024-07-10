24d0a8dc1276473edb05faad4bebbd35b0ad3a77
Zulkar-Jahin <zulkarjahinnizu2000@gmail.com>
2024-07-11 02:14:51 +0600

    Spline Chart (completed)
    *Spline chart added in the dashboard page.
    *Modified DashboardController.cs file for spline chart.
    *Tooltip added to the spline chart.
    *Showing expense and income together in any point.
    
    ** Need to fix the dashboard . its not showing the recent transactions. mainly there may have some erron in the code.
    
    Working fine till now (8273)

--- Expense Tracker/Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a03c88a7d3b166861594e002308957dd45ac83a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27aaf69d91f50064783b9f7c18768ee1580d0005")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
